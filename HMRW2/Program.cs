30c2d560130dad06d2997920b2f87a44fc52cf79
Igor <matefirm@gmail.com>
2022-08-02 14:11:36 +0300

    comm

---
+++ HMRW2/Program.cs
@@ -0,0 +1,54 @@
+﻿//task10
+
+int numberA = new Random ().Next(100,999);
+console.WriteLine(numberA);
+int secNumber = numberA % 100; // int thirdNumber = numberA % 10
+
+console.WriteLine(secNumber);
+
+
+//task13
+
+
+
+int range (int st1, int st2);
+
+{
+    int numberA = new Random ().Next(st1,st2);
+console.WriteLine(numberA);
+
+int thirdNumber = numberA % 10;
+int firstNumber = numberA /100;
+
+
+int result = firstNumber * 10 + thirdNumber;
+return result;
+}
+
+int A = range (10, 999);
+console.WriteLine(A);
+
+
+if (numberA == thirdNumber)
+{
+    Console.WriteLine(numberA);
+}
+else
+
+  Console.WriteLine("No third number");
+
+
+
+//task15
+
+int numberсс = new Random ().Next(1,7);
+console.WriteLine(numbercc);
+
+
+while (numbercc >5)
+{
+    Console.WriteLine("ВЫХОДНОЙ");
+
+};
+
+//pp
